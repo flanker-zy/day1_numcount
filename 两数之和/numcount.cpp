3681f299cc8dfe3dc3ff666350e4ac0b936a9790
flanker-zy <486844956@qq.com>
2021-01-29 15:53:32 +0800

    长度

--- 两数之和/numcount.cpp
+++ 两数之和/numcount.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 
-void function(int nums[], int target, int result[])
+void function(int nums[], int target, int result[],int len)
 {
 	int i, j;
 	int status = 0;
-	for (i = 0; i < sizeof(nums); i++)
+	for (i = 0; i < len; i++)
 	{
 		if (status == 0)
 		{
-			for (j = i + 1; j < sizeof(nums); j++)
+			for (j = i + 1; j < len; j++)
 			{
 				if (nums[i] + nums[j] == target)
 				{
@@ -28,7 +28,7 @@ void function(int nums[], int target, int result[])
 	{
 		cout << "数组中不存在可组成目标值的两数字" << endl;
 	}
-	else if (status = 1)
+	else if (status == 1)
 	{
 		cout << "两数下标分别为：" << result[0] << " " << result[1] << endl;
 	}
@@ -40,8 +40,9 @@ void function(int nums[], int target, int result[])
 int main()
 {
 	int nums[4] = { 2,7,11,13 };
+	int len = 4;
 	int target = 15;
 	int result[2] = { 0 };
-	function(nums, target, result);
+	function(nums, target, result,len);
 	return 0;
 }
